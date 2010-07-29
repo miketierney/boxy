2f5c9409bb1317551d894e1e213391d785ca4255
Andy Hartford <hartforda@gmail.com>
2010-07-29 15:29:28 -0700

    Allow all modals to keep their keypress binding
    Rewrite some of the event binding stuff for keypresses so that the first
    modal dialog won't destroy the bindings of other modals.

--- src/javascripts/jquery.boxy.js
+++ src/javascripts/jquery.boxy.js
@@ -478,11 +478,13 @@ Boxy.prototype = {
                 })).appendTo(document.body);
             this.toTop();
             if (this.options.closeable) {
-                jQuery(document.body).bind('keypress.boxy', function(evt) {
+                this.keypressIsBound = true;
+                jQuery(document).bind('keypress.boxy', function(evt) {
                     var key = evt.which || evt.keyCode;
                     if (key == 27) {
+                        jQuery(document).unbind(evt);
+                        self.keypressIsBound = false;
                         self.hide();
-                        jQuery(document.body).unbind('keypress.boxy');
                     }
                 });
             }
@@ -503,7 +505,9 @@ Boxy.prototype = {
 		var self = this;
         
 		if (this.options.modal) {
-            jQuery(document.body).unbind('keypress.boxy');
+            if (this.keypressIsBound) {
+                jQuery(document).unbind('keypress.boxy');
+            }
             this.modalBlackout.animate({opacity: 0}, function() {
                 jQuery(this).remove();
             });
